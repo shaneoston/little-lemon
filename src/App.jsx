6a1eecfc0ee2909017803aea1c5b844cb7b631c6
Shane <shane@shaneoston.com>
2024-04-28 19:08:37 -0600

    fixes tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import {Footer, Nav} from "./components/index.jsx";
 
 function App() {
   return (
-    <>
+    <div className="grid gap-4 grid-cols-3 grid-rows-3 bg-slate-500">
         {/*<Header />*/}
         <Nav />
         {/*<Main />*/}
         <Footer />
-    </>
+    </div>
   )
 }
 
