0cd9144635c6c4693339a2ee634f2942706a839a
Shane <shane@shaneoston.com>
2024-05-15 17:58:42 -0700

    Improve responsiveness with layout and styling updates
    The restructuring and enhancements made to Main and Hero components help improve the responsiveness of the website. Significant changes include tailored layout modifications for 'lg' size screens, button styling adjustments, image format optimisation and several additional CSS rules for more precise control over elements at different screen sizes.

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,18 +1,20 @@
 export const Hero = () => {
   return (
-    <>
-      <div>
-        <img className="object-cover w-full h-60" src="/src/assets/restauranfood.jpg" alt="food" />
+    <div className="lg:flex">
+      <div className="lg:flex lg:justify-around lg:order-last lg:w-1/2 lg:pt-6 lg:pr-6">
+        <img className="object-cover w-full h-60 lg:rounded-xl"
+             src="/src/assets/restauranfood.jpg"
+             alt="food" />
       </div>
-      <div className="px-6 pb-6 lg:basis-1/2">
-        <h1 className="text-llgold font-serif text-6xl pt-6">Little Lemon</h1>
+      <div className="p-6 lg:order-first lg:w-1/2">
+        <h1 className="text-llgold font-serif text-6xl">Little Lemon</h1>
         <h2 className="text-white font-serif text-3xl">Chicago</h2>
         <p className="text-white text-lg font-sans pb-6">
           We are a family owned Mediterranean restaurant, focused on
           traditional recipes served with a modern twist.
         </p>
-        <button className="bg-llgold w-full px-8 py-3 rounded-lg">Reserve a Table</button>
+        <button className="bg-llgold w-full px-8 py-3 rounded-lg lg:w-1/2">Reserve a Table</button>
       </div>
-    </>
+    </div>
   );
 };
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -2,7 +2,7 @@ import { Hero } from './hero.jsx';
 
 export const Main = () => {
   return (
-    <div className="bg-llgreen">
+    <div className="bg-llgreen lg:h-84">
       <main className="container">
         <Hero />
       </main>
