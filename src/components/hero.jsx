099f8e153e9eed7551e612098811c612174eed3b
Shane <shane@shaneoston.com>
2024-05-21 17:04:17 -0600

    Update layout and styling for Hero component
    This commit enhances the layout and styling of the Hero component. Changes include the addition of a container div, adjustments to button text size and font weight, and introduction of a background color. These modifications aim to improve both desktop and mobile user interfaces.

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,19 +1,22 @@
 export const Hero = () => {
   return (
-    <div className="lg:flex">
-      <div className="lg:flex lg:justify-around lg:order-last lg:w-1/2 lg:pt-6 lg:pr-6">
-        <img className="object-cover w-full h-60 lg:rounded-xl"
-             src="/src/assets/restauranfood.jpg"
-             alt="food" />
-      </div>
-      <div className="p-6 lg:order-first lg:w-1/2">
-        <h1 className="text-llgold font-serif text-6xl">Little Lemon</h1>
-        <h2 className="text-white font-serif text-3xl">Chicago</h2>
-        <p className="text-white text-lg font-sans pb-6">
-          We are a family owned Mediterranean restaurant, focused on
-          traditional recipes served with a modern twist.
-        </p>
-        <button className="bg-llgold w-full px-8 py-3 rounded-lg lg:w-1/2">Reserve a Table</button>
+    <div className="bg-llgreen">
+      <div className="container lg:flex bg-llgreen lg:h-84">
+        <div className="lg:flex lg:justify-around lg:order-last lg:w-1/2 lg:pt-6 lg:pr-6">
+          <img className="object-cover w-full h-60 lg:rounded-xl"
+               src="/src/assets/restauranfood.jpg"
+               alt="food" />
+        </div>
+        <div className="p-6 lg:order-first lg:w-1/2">
+          <h1 className="text-llgold font-serif text-6xl">Little Lemon</h1>
+          <h2 className="text-white font-serif text-3xl">Chicago</h2>
+          <p className="text-white text-lg font-sans pb-6">
+            We are a family owned Mediterranean restaurant, focused on
+            traditional recipes served with a modern twist.
+          </p>
+          <button className="bg-llgold w-full px-8 py-3 text-xl font-semibold rounded-lg lg:w-1/2">Reserve a Table
+          </button>
+        </div>
       </div>
     </div>
   );
