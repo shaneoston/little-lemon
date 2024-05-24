f2826d639c97f60780049ae652c475b50ca751fe
Shane <shane@shaneoston.com>
2024-05-23 20:12:47 -0600

    Add booking form functionality and layout adjustments
    Implemented booking form functionalities allowing users to input reservation details such as date, time, number of guests, and occasion. Refactoring includes the addition of a handleBooking function and improvements to form styles in output.css. Also, adjustments were made to the layout element sizing.

--- src/components/booking/booking-form.jsx
+++ src/components/booking/booking-form.jsx
@@ -1,26 +1,69 @@
-export const BookingForm = () => {
+import { useState } from 'react';
+
+export const BookingForm = ({ handleBooking }) => {
+  const [bookingData, setBookingData] = useState({
+    date: '',
+    time: '17:00',
+    guests: 1,
+    occasion: ''
+  });
+  const [availableTimes] = useState([
+    '17:00',
+    '18:00',
+    '19:00',
+    '20:00',
+    '21:00',
+    '22:00'
+  ]);
+
+  const updateKey = (key, { target: { value } }) => {
+    setBookingData({
+      ...bookingData,
+      [key]: value || null
+    });
+  };
+
   return (
     <>
-      <form className="grid max-w-4xl gap-5">
-        <label htmlFor="res-date">Choose date</label>
-        <input type="date" id="res-date" />
-        <label htmlFor="res-time">Choose time</label>
-        <select id="res-time ">
-          <option>17:00</option>
-          <option>18:00</option>
-          <option>19:00</option>
-          <option>20:00</option>
-          <option>21:00</option>
-          <option>22:00</option>
+      <form className="grid max-w-4xl gap-3 text-2xl mb-12">
+        <label htmlFor="booking-date">Choose date</label>
+        <input className="border border-gray-600 p-6 mb-3"
+               value={bookingData.date}
+               onChange={evt => updateKey('date', evt)}
+               type="date"
+               id="booking-date" />
+        <label htmlFor="booking-time">Choose time</label>
+        <select className="border border-gray-600 p-6 mb-3"
+                id="booking-time"
+                value={bookingData.time}
+                onChange={evt => updateKey('time', evt)}>
+          {availableTimes.map(time => (
+            <option key={time}>{time}</option>
+          ))}
         </select>
         <label htmlFor="guests">Number of guests</label>
-        <input type="number" placeholder="1" min="1" max="10" id="guests" />
+        <input className="border border-gray-600 p-6 mb-3"
+               type="number"
+               placeholder="1"
+               min="1"
+               max="10"
+               id="guests"
+               value={bookingData.guests}
+               onChange={evt => updateKey('guests', evt)}
+        />
         <label htmlFor="occasion">Occasion</label>
-        <select id="occasion">
-          <option>Birthday</option>
-          <option>Anniversary</option>
+        <select className="border border-gray-600 p-6 mb-3"
+                id="occasion"
+                value={bookingData.occasion}
+                onChange={evt => updateKey('occasion', evt)}>
+          <option value=""></option>
+          <option value="birthday">Birthday</option>
+          <option value="anniversary">Anniversary</option>
         </select>
-        <input type="submit" value="Make Your reservation" />
+        <input className="bg-llgold font-sans px-4 py-6 text-xl font-semibold rounded-lg w-full md:w-48 md:h-14"
+               type="button"
+               value="Make Your booking"
+               onClick={handleBooking(bookingData)} />
       </form>
     </>
   );
--- src/pages/booking-page.jsx
+++ src/pages/booking-page.jsx
@@ -1,14 +1,18 @@
 import { BookingForm, BookingHero } from '../components';
 
 export const BookingPage = () => {
+  const handleBooking = (booking => {
+    console.log(booking);
+  });
+
   return (
     <>
       <BookingHero />
       <div className="container max-w-4xl p-6 md:pt-12 sm:p-6">
-        <h1 className="font-serif w-1/2 text-6xl pb-4 md:w-3/4">Make a booking</h1>
+        <h1 className="font-serif w-100 text-6xl">Make a booking</h1>
       </div>
-      <div className="container max-w-4xl p-6">
-        <BookingForm />
+      <div className="container max-w-4xl px-6">
+        <BookingForm handleBooking={handleBooking} />
       </div>
     </>
   );
