c93b27768e7922fa5007d96cdecdac6b709ecc4e
Shane <shane@shaneoston.com>
2024-05-30 17:56:03 -0600

    Refactor CSS and update footer component
    Updated the CSS to change the styling and layout properties for various elements. This includes modifications to margins, padding, font size, and alignment among others. Additionally, the footer component has been updated to reflect changes in layout and navigation links.

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,34 +1,27 @@
-import React from 'react'
-
 export const Footer = () => {
-    return (
-        <footer className="grid grid-cols-2 grid-rows-1">
-            <div>
-                <ul>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">About</a></li>
-                    <li><a href="">Menu</a></li>
-                    <li><a href="">Reservations</a></li>
-                    <li><a href="">Order online</a></li>
-                    <li><a href="">Login</a></li>
-                </ul>
-            </div>
-            <div>
-                <p>Contact</p>
-                <ul>
-                    <li><a href="">1234 Anywhere Street</a></li>
-                    <li><a href="">London NW11 02J</a></li>
-                    <li><a href="">(0)20 7777 8888</a></li>
-                    <li><a href="">food@littlelemon.com</a></li>
-                </ul>
-            </div>
-            <div>
-                <ul>
-                    <li><a href="">Facebook</a></li>
-                    <li><a href="">Instagram</a></li>
-                    <li><a href="">TikTok</a></li>
-                </ul>
-            </div>
-        </footer>
-    )
-}
+  return (
+    <footer className="bg-llgreen">
+      <div className="container max-w-4xl p-6 flex justify-around md:justify-end text-white text-xl">
+        <div className="mr-8">
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Menu</a></li>
+            <li><a href="/booking">Reservations</a></li>
+            <li><a href="">Order online</a></li>
+            <li><a href="">Login</a></li>
+          </ul>
+        </div>
+        <div>
+          <p>Contact</p>
+          <ul>
+            <li><a href="">1234 Anywhere Street</a></li>
+            <li><a href="">London NW11 02J</a></li>
+            <li><a href="">(0)20 7777 8888</a></li>
+            <li><a href="">food@littlelemon.com</a></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+};
